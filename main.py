1289a0dc794b1648e9135da54d5f313bb234dffe
faloger <gtllop098@gmail.com>
2025-09-23 21:47:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = int(input("Количество школьников:"))
-k = int(input("Количество яблок:"))
-apples_in_hand = k // n
-apples_in_basket = k % n
-print (apples_in_hand, apples_in_basket)
+var_init = 70
+var_float = 8.4
+var_str = "No"
+big_int35 = var_init * 3
+var_float -= 1
+var_str = var_str * 3 +"Yes"*2
+print (big_int35, var_float, var_init/var_float, big_int35/var_float, var_str, end="\n" )
