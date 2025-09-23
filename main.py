2c1d231eca247492da13957d777b5dc595bbc876
faloger <gtllop098@gmail.com>
2025-09-23 21:21:03 +0300

    Update main.py
    No

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 var_init = 70
 var_float = 8.4
-var_str = "Yes"
+var_str = "No"
 big_int35 = var_init * 3
 var_float -= 1
 var_str = var_str * 3 +"Yes"*2
 print (big_int35, var_float, var_init/var_float, big_int35/var_float, var_str, end="\n" )
+
 
 
