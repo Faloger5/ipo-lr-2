4bf30ca242a7efbb2e6d2d95dee9706f95460621
faloger <gtllop098@gmail.com>
2025-09-26 19:04:40 +0300

    Create main.py

---
+++ task_4/main.py
@@ -0,0 +1,7 @@
+import math
+x = double(input())
+y = double(input())
+z = double(input())
+b = sqrt(10*(pow(x, 1/3)+pow(x, y+2)))*(pow(asin(z), 2) - abs(x-y))
+print (b)
+                                        
