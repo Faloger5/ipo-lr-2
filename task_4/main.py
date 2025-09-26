435c128ef4f41f4f3c0b04ca2139a875bdba8686
faloger <gtllop098@gmail.com>
2025-09-26 19:16:55 +0300

    Update main.py

--- task_4/main.py
+++ task_4/main.py
@@ -1,8 +1,8 @@
-import cmath 
-x = float(input())
-y = float(input())
-z = float(input())
-b = cmath.sqrt(10*(pow(x, 1/3)+pow(x, y+2)))*(pow(cmath.asin(z), 2) - abs(x-y))
-print (b)
+import cmath #импортируем модуль math
+x = float(input()) #просим пользователя ввести х
+y = float(input()) #просим пользователя ввести у
+z = float(input()) #просим пользователя ввести z
+b = cmath.sqrt(10*(pow(x, 1/3)+pow(x, y+2)))*(pow(cmath.asin(z), 2) - abs(x-y)) #вычисляем b
+print (b) #выводим на экран b
               
                                         
